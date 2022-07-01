c50bae795eae982e6c2dc466aa188219f434d481
adriancleung <adrian.leung@ualberta.ca>
2022-07-01 16:54:25 -0600

    chore: add xcode cloud ci script

---
+++ ios/ci_post_clone.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+export HOMEBREW_NO_INSTALL_CLEANUP=TRUE
+brew install cocoapods
+# have to add node yourself
+brew install node@16
+# link it to the path
+brew link node@16
+
+brew install yarn
+
+# Install dependencies you manage with CocoaPods.
+yarn
+pod install
